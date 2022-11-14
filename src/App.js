6b0e850d19b8f03e933611af94d7dcd23e4c785c
Don Oh <okdonguk0@gmail.com>
2022-11-13 21:14:22 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import Navbar from './components/general/Navbar';
 import Welcome from './pages/Welcome';
 
 function App() {
-    const location = useLocation();
     const [page, setPage] = useState(false);
 
-    if(page || location.pathname !== "/"){return (
+    if(page || window.location.pathname !== "/"){return (
         <div class="bg-wood-pattern bg-wood-5 fixed w-screen">
             <Navbar />
         </div>
